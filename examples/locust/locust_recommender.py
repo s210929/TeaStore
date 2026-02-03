efdb8b73fd70a0b3d69a6edaa70b73c1f6ad76ee
s210929 <s210929@th-ab.de>
2026-02-03 07:02:37 +0100

    fix loadtest

--- examples/locust/locust_recommender.py
+++ examples/locust/locust_recommender.py
@@ -9,11 +9,13 @@ class RecommenderUser(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def get_recommendation(self) -> None:
-        user_id = random.randint(1, 99)
-        data = {"item": 1,
-                "uid": user_id}
-        response = self.client.post("/tools.descartes.teastore.recommender/rest/recommendsingle", json={})
+    def recommend_single(self) -> None:
+
+        content = {
+            "productId": random.randint(1, 100),
+            "quantity": 1
+        }
+        response = self.client.post("/tools.descartes.teastore.recommender/rest/recommendsingle", params={"uid": random.randint(1, 99)}, json=content, name="singlerecommend")
         if (response.ok):
             logging.info(f"Response ok: {response.status_code}")
         else:
