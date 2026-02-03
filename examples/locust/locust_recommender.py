f755cb2125917e1dd115efa118c0b63cf5d5904c
s210929 <s210929@th-ab.de>
2026-02-03 06:00:31 +0100

    fix yaml

--- examples/locust/locust_recommender.py
+++ examples/locust/locust_recommender.py
@@ -1,12 +1,17 @@
 from locust import HttpUser, task, between
 import random
+import logging
+
+# logging
+logging.getLogger().setLevel(logging.INFO)
 
 class RecommenderUser(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def get_recommendation(self):
+    def get_recommendation(self) -> None:
         user_id = random.randint(1, 1000)
+        logging.info("Get recommendation")
         with self.client.get(
             f"/recommendation/{user_id}",
             name="/recommendation/{userId}",
@@ -14,3 +19,5 @@ class RecommenderUser(HttpUser):
         ) as response:
             if response.status_code != 200:
                 response.failure(f"Status {response.status_code}")
+                logging.info("Recommendation failed")
+        logging.info("Recommendation complete")
