ab1259acea514b8df59359df5154da148f60b28a
s210929 <s210929@th-ab.de>
2026-05-06 14:16:09 +0200

    adjust locust

--- examples/locust/locust_recommender.py
+++ examples/locust/locust_recommender.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, LoadTestShape
 import random
 import logging
 
@@ -10,7 +10,6 @@ class RecommenderUser(HttpUser):
 
     @task
     def recommend_single(self) -> None:
-
         content = {
             "productId": random.randint(1, 100),
             "quantity": 1
@@ -20,3 +19,21 @@ class RecommenderUser(HttpUser):
             logging.info(f"Response ok: {response.status_code}")
         else:
             logging.info(f"Response not ok: {response.status_code}")
+
+
+class StepLoadShape(LoadTestShape):
+    step_time = 60
+    step_load = 10
+    max_steps = 10
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        current_step = int(run_time / self.step_time) + 1
+        if current_step > self.max_steps:
+            return None
+
+        user_count = current_step * self.step_load
+        spawn_rate = self.step_load
+
+        return (user_count, spawn_rate)
